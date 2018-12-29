211d8e561c55c876c16ce03b827e2dc0f34f0c44
加藤　優奈 <yuvernacrew@gmail.com>
2018-12-29 13:21:19 +0900

    update: rails周りの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def page_title
+    title = "moshamosha" #これがサイトのブランド名
+    title = @page_title + " | " + title if @page_title
+    return title
+  end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# scss precompile
+Rails.application.config.assets.precompile += %w( moshamosha/* )
+Rails.application.config.assets.precompile += %w( sango/* )
